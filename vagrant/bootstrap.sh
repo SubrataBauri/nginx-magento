2062e6cd89e1dbfc6490ebca9285377e7a3c51fd
Subrata Bauri <subrata@digitalaptech.com>
2020-11-07 01:16:18 +0530

    Create bootstrap.sh file

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+echo "=========================================="
+echo "Updating Ubuntu"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Git
+#echo "Installing Git"
+#sudo apt-get install git -y > /dev/null
+
+# Apache
+echo "Installing nginx"
+sudo apt-get install nginx -y > /dev/null
+
+# PHP
+echo "Updating PHP repository"
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update -y
+
+echo "Installing PHP"
+sudo apt-get install php7.4 -y
+
+echo "Installing PHP extensions"
+## The following line is not tested
+# sudo apt-get install php7.4-common php7.4-dev php5-cli php7.4-fpm -y > /dev/null
+#sudo apt-get install -y libapache2-mod-php7.4 
+sudo apt-get install -y php7.4-cli php7.4-common php7.4-mbstring
+sudo apt-get install -y php7.4-gd php7.4-intl php7.4-simplexml
+sudo apt-get install -y php7.4-zip php7.4-bcmath php-xdebug
+sudo apt-get install -y php7.4-curl php7.4-ctype php7.4-dom php7.4-hash php7.4-iconv php7.4-openssl
+sudo apt-get install -y php7.4-soap php7.4-curl hp7.4-mysql php7.4-pdo_mysql php7.4-xsl
+#sudo a2enmod rewrite
+#sudo service apache2 restart
+
+# echo "Creating xdebug log directory: /var/log/xdebug"
+# sudo mkdir /var/log/xdebug > /dev/null
+# echo "Changing xdebug log directory owner to www-data"
+# sudo chown www-data:www-data /var/log/xdebug > /dev/null
+
+# echo "Installing xdebug"
+# sudo pecl install xdebug > /dev/null
+# #echo "Configuring xdebug"
+# #sudo cp /var/www/html/config/php.ini /etc/php5/apache2/php.ini > /dev/null
+# sudo service apache2 restart > /dev/null
+# echo "Xdebug installation completeted"
+
+# MySQL 
+# echo "Preparing MySQL"
+# sudo apt-get install debconf-utils -y > /dev/null
+# sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+# sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+
+# echo "Installing MySQL"
+# sudo apt-get install mysql-server-5.6 -y > /dev/null
+# sudo apt-get install mysql-server -y -f > /dev/null
+# sudo apt-get install -y mysql-client > /dev/null
+
+
+# Apache Configuration
+# echo "Configuring Apache"
+# cp /var/www/html/config/servername.conf /etc/apache2/conf-available/servername.conf > /dev/null
+# sudo a2enconf servername > /dev/null
+# cp /var/www/html/config/dir.conf /etc/apache2/mods-enabled/dir.conf > /dev/null
+# sudo service apache2 restart > /dev/null
+
+# Install Git
+# echo "Installing Git"
+# sudo apt-get install git -y
+
+# Installing Composer
+echo "Installing Composer"
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Installing MariaDB 10.4
+echo "Installing MariaDB 10.4"
+sudo apt-get install udo
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://mariadb.mirror.liquidtelecom.com/repo/10.4/ubuntu $(lsb_release -cs) main"
+sudo apt-get update
+sudo -y apt-get install mariadb-server mariadb-client
+sudo mysql_secure_installation
+
+sudo systemctl status mariadb
+
+# Installing Node
+# echo "Installing Node"
+# sudo curl -sL https://deb.nodesource.com/setup | sudo bash - > /dev/null
+# sudo sudo apt-get install -y nodejs > /dev/null
+# sudo npm install npm -g
+
+# echo "Installing Bower"
+# sudo npm install bower -g > /dev/null
+
+# echo "Installing Gulp"
+# sudo npm install Gulp -g > /dev/null
+
+echo "Install Elasticsearch"
+sudo apt-get update -y
+sudo apt-get install apt-transport-https -y
+sudo apt-get install openjdk-8-jdk -y
+jave -version
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
+sudo apt-get update
+sudo apt-get install elasticsearch
+sudo systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+curl -X GET "localhost:9200/"
+sudo journalctl -u elasticsearch
+
+
+# sudo curl -XGET '<host>:9200/_cat/health?v&pretty'
+
+echo "Finished provisioning."
